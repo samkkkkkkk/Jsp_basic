b0537aba38f88aca2c52791896b0ffe87efa22a2
samkyung <rnflrntk@naver.com>
2023-09-12 17:48:07 +0900

    20230912 jsp공부

--- JspBasic/src/main/java/com/jsp/servlet/SessionServlet.java
+++ JspBasic/src/main/java/com/jsp/servlet/SessionServlet.java
@@ -20,12 +20,20 @@ public class SessionServlet extends HttpServlet {
         super();
        
     }
-
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		System.out.println("로그아웃 요청이 들어옴!");
+		//특정 세션 데이터를 삭제하는 메서드
+//		request.getSession().removeAttribute("user_id");
+		
+		//모든 세션 데이터 삭제하는 법(세션 객체 자체를 무효화)
+		request.getSession().invalidate();
+		
+		response.sendRedirect("/JspBasic/session/session_login.jsp");
+		
+		
 	}
-
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");//문자열 인코딩
@@ -44,8 +52,11 @@ public class SessionServlet extends HttpServlet {
             */
 			
 			HttpSession session = request.getSession();
-		
 			session.setAttribute("user_id", id);			
+			
+			//세션의 유효 시간 설정
+			//세션의 수명은 새로운 요청이 서버로 들어오면 초기화됩니다.
+			session.setMaxInactiveInterval(60 * 60 * 24 * 30); //초 단위의 세션 유효시간 설정.
 			
 			response.sendRedirect("/JspBasic/session/session_welcome.jsp");
 			
@@ -66,9 +77,7 @@ public class SessionServlet extends HttpServlet {
             w.flush();
             w.close();
 		}
-		
-		
-		
+	
 	}
 
 }
